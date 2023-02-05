1bb7e56854bb995c838c065207c8f06ad11bcb39
pablo escaida <1pablo.escaida1@gmail.com>
2023-02-05 20:36:35 +0100

    readme and date-transform

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# html
+#Intro
+
+mysql -u root -p statistiki < file.sql
+
+sudo apt install libapache2-mod-php
+
+sudo apt-get install php-mysql
+
+cd /etc/php/7.2/apache2
+
+sudo nano php.ini
+
+(uncomment mysqli module)
+
+sudo /etc/init.d/apache2 restart
+
+add phpinfo(); to a php file and search for mysqli
+
+
+
+CREATE USER 'pablo'@'localhost' IDENTIFIED BY 'portaf';
+
+GRANT ALL PRIVILEGES ON *.* TO 'pablo'@'localhost' WITH GRANT OPTION;
--- dashb/date-transform.php
+++ dashb/date-transform.php
@@ -16,9 +16,26 @@
 
 <?php
 
+
 echo "Hola<br><hr>";
 
-$mysqli = mysqli_connect("sql108.byethost7.com", "b7_24447646", "Palmtree!00", "b7_24447646_statistiki");
+$mysqli = mysqli_connect("localhost", "pablo", "portaf", "statistiki");
+
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+/* check if server is alive */
+if ($mysqli->ping()) {
+    printf ("Our connection is ok!\n");
+} else {
+    printf ("Error: %s\n", $mysqli->error);
+}
+
+
 $res = mysqli_query($mysqli, "select stamp, UNIX_TIMESTAMP() as ahora from availability_today order by stamp desc limit 10");
 
 $row = $res->fetch_assoc();
@@ -29,20 +46,24 @@ $currstamp = $row["ahora"];
 $targetstamp = strtotime('last Friday 9:28:47');
 $ddiff = $targetstamp - $oldstamp;
 
+echo "<hr>oldstamp:";
 echo $oldstamp;
 echo '<br>';
 echo date("l M j, Y, H:i:s",$oldstamp);
 echo "<hr>";
 
+echo "<hr>currstamp:";
 echo $currstamp;
 echo '<br>';
 echo date("l M j, Y, H:i:s",$currstamp);
 echo "<hr>";
 
+echo "<hr>targetstamp:";
 echo $targetstamp;
 echo '<br>';
 echo date("l M j, Y, H:i:s",$targetstamp);
 echo "<hr>";
+echo "<hr>diff:";
 echo $ddiff;
 echo "<hr>";
 
@@ -55,7 +76,10 @@ echo 'close';
 <body>
 <pre>
 run in phpmyadmin
-UPDATE `availability_today` SET `stamp`=`stamp`+127133290
+
+use statistiki
+UPDATE `availability_today` SET `stamp`=`stamp`+<?php echo $ddiff; ?>;
+
 $mysqli = mysqli_connect("sql108.byethost7.com", "b7_24447646", "Palmtree!00", "b7_24447646_statistiki");
 sort desc stamp in phpmyadmin and adjust for written time
 
